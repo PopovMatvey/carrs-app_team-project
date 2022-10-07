c5cd81b84bd5dfe47cbe34f3ce8ea7d9435601c1
матвей попов <matyha550@gmail.com>
2022-10-07 13:09:02 +0300

    release-client

---
+++ precache-manifest.168ff71f413cb7a6c1648652f2622583.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "0971641c6afacf044de9",
+    "url": "/static/css/main.7e0fa64f.chunk.css"
+  },
+  {
+    "revision": "0971641c6afacf044de9",
+    "url": "/static/js/main.0971641c.chunk.js"
+  },
+  {
+    "revision": "bc080ad008275de84472",
+    "url": "/static/css/1.594031be.chunk.css"
+  },
+  {
+    "revision": "bc080ad008275de84472",
+    "url": "/static/js/1.bc080ad0.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2e527cec4d7fbe80b5bfd0638f033231",
+    "url": "/static/media/logo.2e527cec.png"
+  },
+  {
+    "revision": "b9fb6d6603fbb54d8f9d478c77008808",
+    "url": "/static/media/rolls.b9fb6d66.png"
+  },
+  {
+    "revision": "c613ca81d36649b260700f3335d8f579",
+    "url": "/static/media/Roboto.c613ca81.ttf"
+  },
+  {
+    "revision": "1fc6a83d1e366dfe7babb690d33ba6fb",
+    "url": "/static/media/Robotobold.1fc6a83d.ttf"
+  },
+  {
+    "revision": "91a5c93a35e51736c8b54b4a234a093d",
+    "url": "/static/media/background.91a5c93a.png"
+  },
+  {
+    "revision": "74f85784db65254d5c7c1302ecff476f",
+    "url": "/index.html"
+  }
+];
